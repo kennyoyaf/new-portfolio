b687dc3b7d3225b22de79c29a9f36badeadc2637
kennyoyaf <kehindeoyafemi@gmail.com>
2025-02-28 16:10:49 +0100

    commit

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
 
 const ServiceCard = ({ name, description }) => {
   const { theme } = useTheme();
@@ -11,10 +11,10 @@ const ServiceCard = ({ name, description }) => {
   return (
     <div
       className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
-        mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
+        mounted && theme === 'dark' ? 'hover:bg-slate-800' : 'hover:bg-slate-50'
       } hover:scale-105 link`}
     >
-      <h1 className="text-3xl">{name ? name : "Heading"}</h1>
+      <h1 className="text-3xl">{name ? name : 'Heading'}</h1>
       <p className="mt-5 opacity-40 text-xl">
         {description
           ? description
